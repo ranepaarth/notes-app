88c2b3155237dfd9bbc47441e6c6a2355cf04797
ranepaarth <2019paarth.rane@ves.ac.in>
2023-12-26 21:33:34 +0530

    backend: notes CRUD functionality

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -1,24 +1,43 @@
 const Notes = require("../models/notes.models");
 const getAllNotesController = async (req, res) => {
-  const notes = await Notes.find({});
-  res.status(200).json(notes);
+  try {
+    const notes = await Notes.find({}).sort({ _id: -1 });
+    res.status(200).json(notes);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const createNotesController = async (req, res) => {
-  const newNote = await Notes.create(req.body);
-  res.status(200).json(newNote);
+  const { title, content } = req.body;
+  try {
+    const newNote = await Notes.create({ title, content });
+    console.log(req.body);
+    res.status(200).json(newNote);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const updateNotesController = async (req, res) => {
   const { id } = req.params;
-  const updatedNotes = await Notes.findByIdAndUpdate(id, { $set: req.body });
-  res.status(200).json(updatedNotes);
+  try {
+    const updatedNotes = await Notes.findByIdAndUpdate(id, { $set: req.body });
+    console.log(updatedNotes);
+    res.status(200).json(updatedNotes);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const deleteNotesController = async (req, res) => {
   const { id } = req.params;
-  const deletedNotes = await Notes.findByIdAndDelete(id);
-  res.status(200).json(deletedNotes);
+  try {
+    const deletedNotes = await Notes.findByIdAndDelete(id);
+    res.status(200).json(deletedNotes);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = {
