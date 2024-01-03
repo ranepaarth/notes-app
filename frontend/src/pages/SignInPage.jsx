2553e762c0b77e88ed7efee084d9939c77404a95
ranepaarth <2019paarth.rane@ves.ac.in>
2024-01-03 21:10:04 +0530

    frontend: test user info

--- frontend/src/pages/SignInPage.jsx
+++ frontend/src/pages/SignInPage.jsx
@@ -49,7 +49,7 @@ const SignInPage = () => {
           <FormPinIcon />
           <FormHeader
             heading={"Log in to your account"}
-            subheader={`Don't have an account?`}
+            subheader="Don't have an account?"
             linkTo={"/signup"}
             linkToPage={"Create new account"}
           />
@@ -57,7 +57,10 @@ const SignInPage = () => {
             handleSubmit={handleSubmit}
             onFormSubmit={onFormSubmit}
           >
-            <EmptyLine width={"w-[88%]"} />
+            <span className="flex items-center text-sm bg-yellow-950/10 h-8 px-2 font-medium text-yellow-950/90 divide-x divide-yellow-950/40">
+              <span className="px-2">test3@test.com</span>
+              <span className="px-2">Test@123</span>
+            </span>
             <FormInput
               Icon={<MdOutlineAlternateEmail />}
               type={"text"}
