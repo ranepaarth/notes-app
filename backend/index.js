b33fb4dde5f093432d2cb7d610940fec68d92574
ranepaarth <2019paarth.rane@ves.ac.in>
2024-01-19 13:24:12 +0530

    backend: cors

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,27 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin",
+    process.env.CLIENT_URL);
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Pass to next layer of middleware
+  next();
+});
+app.options('*', cors())
 app.use(cors());
 app.use(express.json());
 app.use("/api/notes", notesRouter);
