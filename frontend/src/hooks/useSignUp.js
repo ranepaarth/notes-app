3403fd040e5c819a18eed066dca3c563ee8b84c6
ranepaarth <2019paarth.rane@ves.ac.in>
2024-01-19 12:51:30 +0530

    frontend: prettier formatting

--- frontend/src/hooks/useSignUp.js
+++ frontend/src/hooks/useSignUp.js
@@ -3,36 +3,36 @@ import { useState } from "react";
 const useSignUp = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [success,setSuccess] = useState(false)
+  const [success, setSuccess] = useState(false);
 
   const signupUser = async (data) => {
-    setLoading(true)
-    setError(null)
+    setLoading(true);
+    setError(null);
 
     const response = await fetch("http://localhost:4000/api/users/signup", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     });
 
-    const responseData = await response.json()
+    const responseData = await response.json();
 
-    if(!response.ok) {
-        setLoading(false)
-        setError(responseData.error)
-        setSuccess(false)
+    if (!response.ok) {
+      setLoading(false);
+      setError(responseData.error);
+      setSuccess(false);
     }
 
-    if(response.ok){
-        setError(null)
-        setLoading(false)
-        setSuccess(true)
+    if (response.ok) {
+      setError(null);
+      setLoading(false);
+      setSuccess(true);
     }
   };
 
-  return {signupUser,error,loading,success}
+  return { signupUser, error, loading, success };
 };
 
 export default useSignUp;
